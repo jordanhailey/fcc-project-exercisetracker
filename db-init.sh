191f51134cb1b2a21ae1affd5056992ed714f3ee
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-08-11 04:10:19 +0000

    database initializer

---
+++ db-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo '{
+  "users": [
+    {
+      "username": "jordan",
+      "exercise_log": [
+        {
+          "description": "build this exercise tracker",
+          "duration": "60",
+          "date": "2021-08-10"
+        }
+      ]
+    }
+  ]
+}
+' > db.json
